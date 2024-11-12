782ddf8fbd5882b4d0b7623b7566613e7cf7cbf2
nate-maxwell <natedmaxwell@gmail.com>
2024-11-12 15:54:09 -0800

    documentation

--- er.go
+++ er.go
@@ -7,6 +7,14 @@ import "fmt"
 // The error must not equal nil.
 // For use with functions that only return an error or nil.
 // Will panic if err != nil.
+//
+// Instead of writing:
+// >>> err := someFunc(args...)
+// >>> if err != nil {}
+//
+// You can write:
+// >>> utils.ErrMust(someFunc(args...))
+// and it will panic if the error is not nil.
 func ErrMust(err error) {
 	if err != nil {
 		fmt.Println(err)
@@ -16,6 +24,14 @@ func ErrMust(err error) {
 
 // Panics if the error of a function that returns a tuple (val, err)
 // returned a non-nil error, otherwise will return the value.
+//
+// Instead of writing:
+// >>> foo, err := someFunc(args...)
+// >>> if err != nil {}
+//
+// You can write:
+// >>> foo := utils.RetMust(someFunc(args...))
+// and it will panic if the error is not nil.
 func RetMust[T any](value T, err error) T {
 	if err != nil {
 		fmt.Println(err)
