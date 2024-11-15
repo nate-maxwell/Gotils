7dc4e92b44d382d00947f27b728efdf7ec9d0ee6
nate-maxwell <natedmaxwell@gmail.com>
2024-11-15 13:44:51 -0800

    moved some file funcs from dir to file

--- dir/dir.go
+++ dir/dir.go
@@ -174,37 +174,6 @@ func DeleteSafeFilesInDirectory(folderPath pathlib.Path) error {
 	return nil
 }
 
-// Copy file into a separate destination folder.
-//
-// Args:
-//
-//	source(pathlib.Path): File path of the file to copy.
-//	dest(pathlib.Path): File path to copy the file too, optionally can have different name.
-//
-// Returns:
-//
-//	error: *PathError crated from os module or possible other error from io module else nil.
-func CopyFile(source pathlib.Path, dest pathlib.Path) error {
-	sourceFile, err := os.Open(source.String())
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
-
-	destFile, err := os.Create(dest.String())
-	if err != nil {
-		return err
-	}
-	defer destFile.Close()
-
-	_, err = io.Copy(destFile, sourceFile)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Copy contents of a folder to the given destination.
 //
 // Args:
@@ -245,45 +214,6 @@ func CopyFolderContents(sourcePath pathlib.Path, destination pathlib.Path) error
 				return err
 			}
 		}
-	}
-	return nil
-}
-
-// Exports a string map to json file path.
-//
-// Args:
-//
-//	filepath(pathlib.Path): The file path to place the .json file.
-//	data(map[string]interface{}): Any map with string keys and values that can be converted to strings.
-//	overWrite(bool): To overwrite json file if it already exists in path.
-//
-// Returns:
-//
-//	error: Any relevant error from the json handling or file writing process.
-func ExportMapToJson(filePath pathlib.Path, data map[string]interface{}, overWrite bool) error {
-	exists, err := filePath.Exists()
-	if err != nil {
-		return err
-	}
-
-	if !exists || overWrite {
-		jsonData, err := json.Marshal(data)
-		if err != nil {
-			return err
-		}
-
-		file, err := os.Create(filePath.String())
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		_, err = file.Write(jsonData)
-		if err != nil {
-			return err
-		}
-
-		return nil
 	}
 	return nil
 }
--- file/file.go
+++ file/file.go
@@ -1,6 +1,9 @@
 package file
 
 import (
+	"encoding/json"
+	"io"
+	"os"
 	"path/filepath"
 
 	"github.com/chigopher/pathlib"
@@ -16,4 +19,74 @@ var imageFileExtensions = []string{".jpg", ".jpeg", ".png", ".tif", ".tiff", ".i
 func IsImageFile(filePath pathlib.Path) bool {
 	ext := filepath.Ext(filePath.String())
 	return arr.StringSliceContains(imageFileExtensions, ext)
+}
+
+// Copy file into a separate destination folder.
+//
+// Args:
+//
+//	source(pathlib.Path): File path of the file to copy.
+//	dest(pathlib.Path): File path to copy the file too, optionally can have different name.
+//
+// Returns:
+//
+//	error: *PathError crated from os module or possible other error from io module else nil.
+func CopyFile(source pathlib.Path, dest pathlib.Path) error {
+	sourceFile, err := os.Open(source.String())
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dest.String())
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Exports a string map to json file path.
+//
+// Args:
+//
+//	filepath(pathlib.Path): The file path to place the .json file.
+//	data(map[string]interface{}): Any map with string keys and values that can be converted to strings.
+//	overWrite(bool): To overwrite json file if it already exists in path.
+//
+// Returns:
+//
+//	error: Any relevant error from the json handling or file writing process.
+func ExportMapToJson(filePath pathlib.Path, data map[string]interface{}, overWrite bool) error {
+	exists, err := filePath.Exists()
+	if err != nil {
+		return err
+	}
+
+	if !exists || overWrite {
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Create(filePath.String())
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		_, err = file.Write(jsonData)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+	return nil
 }
